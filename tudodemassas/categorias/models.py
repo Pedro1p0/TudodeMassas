b095d463425f6d85009aa65071e72b62f7381389
Pedro Vitor <pedro1p0@outlook.com>
2022-10-18 08:59:42 -0300

    tirei categorias

--- tudodemassas/categorias/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Categoria(models.Model):
-    nome_categoria = models.CharField(max_length=100)
--- tudodemassas/tudodemassas/settings.py
+++ tudodemassas/tudodemassas/settings.py
@@ -32,7 +32,6 @@ LOGIN_URL = 'login/'
 # Application definition
 
 INSTALLED_APPS = [
-    'categorias',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
